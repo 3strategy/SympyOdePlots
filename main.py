e8a8ac925a885130929ca022173a62efa8acfce1
Guy Siedes <3strategy@gmail.com>
2021-08-23 08:50:25 +0300

    plotting 2 functions (latex rendering of eq does not work as well as in Colab or Jupyter)

--- main.py
+++ main.py
@@ -2,3 +2,19 @@ from sympy import *
 import math
 from numpy import linspace
 import matplotlib.pyplot as pl
+from IPython.display import display
+
+x=symbols('x')
+f = (x**2+1)/(x-1)
+display(f)  #this does not display Latex Rendering on pycharm output
+# f=sin(x)  # f נגדיר את
+g=cos(x)
+display(f,g)
+fx=lambdify(x,f,modules=['numpy'])
+gx=lambdify(x,g,modules=['numpy'])
+xVals=linspace(-10,10,100)
+pl.plot(xVals,fx(xVals))
+pl.plot(xVals,gx(xVals))
+pl.xlabel('x')
+pl.ylabel(f)
+pl.show()
